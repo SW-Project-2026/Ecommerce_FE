cc35dc275fee85d50584c7bf7cbf2136d3f890e7
omw011 <omw011@naver.com>
2026-06-04 12:56:55 +0900

    쿠폰 클레임 페이지 URL 진입 라우팅 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import CampaignListPage from './pages/CampaignListPage'
+import CouponClaimPage from './pages/CouponClaimPage'
 
 function getCookie(name) {
   return document.cookie.split('; ').reduce((acc, part) => {
@@ -10,19 +11,27 @@ function getCookie(name) {
   }, null)
 }
 
-const isAdminHash = window.location.hash === '#admin'
-const role        = localStorage.getItem('role')
-const token       = getCookie('accessToken')
+const isAdminHash  = window.location.hash === '#admin'
+const role         = localStorage.getItem('role')
+const token        = getCookie('accessToken')
+const hasCouponToken = new URLSearchParams(window.location.search).has('token')
 
-// /#admin 해시이거나, 토큰+ADMIN role이 있으면 관리자 페이지 진입
+// /#admin 해시이거나, role이 ADMIN이면 관리자 페이지 진입
 const isAdmin = isAdminHash || (token && role === 'ADMIN')
 
 if (isAdmin && !isAdminHash) {
   window.location.hash = '#admin'
 }
 
+let root
+if (hasCouponToken) {
+  root = <CouponClaimPage />
+} else if (isAdmin) {
+  root = <CampaignListPage />
+} else {
+  root = <App />
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {isAdmin ? <CampaignListPage /> : <App />}
-  </StrictMode>
+  <StrictMode>{root}</StrictMode>
 )
